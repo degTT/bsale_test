5adf7ad8a9c5139c7da5b661aed3347bc9c1df20
Diego <diego_1995@outlook.cl>
2021-01-26 02:12:42 -0300

    composer.lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '18d44ea10bfc093157bea48f5aacea2c136eae21',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '18d44ea10bfc093157bea48f5aacea2c136eae21',
+    ),
+  ),
+);
